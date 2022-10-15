bdb6a00cf91bc8631e1e41be9d70c87de3368bb8
pedejeca135 <pjco90@hotmail.com>
2022-10-14 21:36:56 -0500

    parte de la velocidad controlada por el potenciometro

--- potenciometro_PWD_modul/potenciometro_PWD_modul.ino
+++ potenciometro_PWD_modul/potenciometro_PWD_modul.ino
@@ -1,9 +1,24 @@
+int avance = 8;
+int retroceso = 9 ;
+int enableMotor = 10;
+int speedMotor = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(avance, HIGH);
+  digitalWrite(avance,LOW);
+  speedMotor = analogRead(A0);
+  speedMotor = speedMotor * (255/1023); 
+  analogWrite(enableMotor , speedMotor);
 }
+
+/*1023 = 255
+x = x*255/1023
+*/
